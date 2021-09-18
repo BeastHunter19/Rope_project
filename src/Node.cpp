177aa4b57b0ad99037e35890b00eb29a5aa1438e
Francesco Coppola <ciccio-peppe@live.com>
2021-09-18 12:58:54 +0200

    Added Node implementation

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,1 +1,45 @@
 #include "Node.h"
+
+Node::Node(): len(0), weight(0), left(nullptr), right(nullptr), 
+ parent(nullptr), str("")
+{}
+
+Node::~Node()
+{
+	if(this->left) delete this->left;
+	if(this->right) delete this->right;
+}
+
+void Node::init(std::string& s)
+{
+	this->parent = nullptr;
+	if (s.length() <= MAX_STR_LEN) {
+		this->left = this->right = nullptr;
+		this->weight = this->len = s.length();
+		this->str = s;
+	}
+	else {
+		std::string l = s.substr(0, s.length()/2);
+		std::string r = s.substr(s.length()/2);
+		this->left = new Node;
+		this->left->init(l);
+		this->left->parent = this;
+		this->right = new Node;
+		this->right->init(r);
+		this->right->parent = this;
+		this->weight = this->left->len;
+		this->len = s.length();
+		this->str = "";
+	}
+}
+
+void Node::init(Node* l, Node* r)
+{
+	this->left = l;
+	this->left->parent = this;
+	this->right = r;
+	this->right->parent = this;
+	this->weight = this->left->len;
+	this->len = this->left->len + this->right->len;
+	this->str = "";
+}
--- src/Node.h
+++ src/Node.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+
 class Node
 {
+public:
+	Node();
+	~Node();
+	void init(std::string& s);
+	void init(Node* l, Node* r);
+	
+	Node* parent;
+	Node* left;
+	Node* right;
+	int weight;
+	int len;
+	std::string str;
+
+private:
+	static constexpr int MAX_STR_LEN = 10;
 };
 
